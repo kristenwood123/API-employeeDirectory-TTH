b5480418d99c69d514b3a720bf1a429a3208da28
Kristen <kristenwood123@gmail.com>
2021-04-22 14:44:27 -0700

    added html

---
+++ app.js
@@ -0,0 +1,13 @@
+
+// const xhr = new XMLHttpRequest()
+
+
+// xhr.onreadystatechange = function() {
+//   if(xhr.readyState === 4) {
+//     document.querySelector('.grid-container').innerHTML = xhr.responseText;
+//   }
+// };
+// xhr.open('GET', 'https://randomuser.me/api/?results=12');
+// xhr.send()
+
+
