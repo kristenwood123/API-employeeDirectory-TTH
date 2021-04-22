c5979964ab5189b5ebd21f31623dd264cb2b07fa
Kristen <kristenwood123@gmail.com>
2021-04-22 15:14:35 -0700

    added global variables

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+const modalBtn = document.querySelector('.modal-close')
+const overlay = document.querySelector('.overlay')
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`
 
-// const xhr = new XMLHttpRequest()
-
-
-// xhr.onreadystatechange = function() {
-//   if(xhr.readyState === 4) {
-//     document.querySelector('.grid-container').innerHTML = xhr.responseText;
-//   }
-// };
-// xhr.open('GET', 'https://randomuser.me/api/?results=12');
-// xhr.send()
+let employees = []
 
 
+
+const xhr = new XMLHttpRequest()
+
+
+xhr.onreadystatechange = function() {
+  if(xhr.readyState === 4) {
+    document.querySelector('.grid-container').innerHTML = xhr.responseText;
+  }
+};
+xhr.open('GET', urlAPI);
+xhr.send()
+
+
+
+
+
+modalBtn.addEventListener('click', () => {
+  overlay.classList.add('hidden')
+})
