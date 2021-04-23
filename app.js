76bfee2784f07338abac5ba973a3b0b46784dc32
Kristen <kristenwood123@gmail.com>
2021-04-23 14:23:10 -0700

    added fetch api

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
 const modalBtn = document.querySelector('.modal-close')
 const overlay = document.querySelector('.overlay')
-const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`
-
+const APIUrl = `https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`
+const gridContainer = document.querySelector('.grid-container')
 // let employees = []
 
 
+function fetchData(url) {
+  return fetch(url)
+    .then(res => res.json())
+}
 
-const xhr = new XMLHttpRequest()
-
-xhr.onreadystatechange = function() {
-  if(xhr.readyState === 4) {
-    if(xhr.status === 200) {
-     let myObj = JSON.parse(xhr.responseText);
-      let employees = myObj.results
-      let cardHTML = '<div class="card">'
-      for(let i = 0; i < employees.length; i++) {
-        cardHTML += `<img src=${employees[i].picture.large} alt=${employees[i].name.first}></div>`;
-        document.querySelector('.grid-container').innerHTML = cardHTML
-        console.log(cardHTML)
-      }
-    } 
+fetchData(APIUrl)
+  .then(data => displayData(data))
+  
+  
+  const displayData = (data) => {
+    const html = `
+    <p>${data.results[1].email}</p>
+    `
+    gridContainer.innerHTML = html
+    console.log(data)
   }
-};
-xhr.open('GET', urlAPI);
-xhr.send()
+
+
+  function fetchModalImage() {
+    const img = card.querySelector('img')
+    const p = card.querySelector('p')
+
+    fetchData(APIUrl)
+  }
 
 modalBtn.addEventListener('click', () => {
   overlay.classList.add('hidden')
 })
-
-//  <div class="text-container">
-//           <h2 class="name">Haleigh Macchiraell</h2>
-//           <p class="emial">dtucker@asdsd.com</p>
-//           <p class="address">Chicago</p>
-//         </div>
+      //  <div class="card">
+      //     <img class="avatar" src="member-1.jpg" />
+      //     <div class="text-container">
+      //       <h2 class="name">Haleigh Macchiarella</h2>
+      //       <p class="email">dtucker@yakitri.edu</p>
+      //       <p class="address">Chicago</p>
+      //    </div>
+      //   </div>
